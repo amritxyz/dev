c4453e2938f677e698be78874c7fb25acef0ce57
amrit-44404 <amrit44404@proton.me>
2024-08-15 18:39:03 +0545

    new JFF stuffs

---
+++ array/4-odd_or_even.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+    int num[5];
+    int i, even=0, odd=0;
+    float avg;
+    printf("Enter 5 Numbers :\n");
+    for (i = 0; i < 5; ++i) {
+        printf("Number %d: ", i + 1);
+        scanf("%d", &num[i]);
+	if(num[i]%2==0){
+		//printf("The number is even\n");
+		even+=1;
+	}
+	else {
+		//printf("The number is odd\n");
+		odd+=1;
+	}
+    }
+    printf("There are %d even numbers\n", even);
+    printf("There are %d odd numbers\n", odd);
+}
+
---
+++ fun/void.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main() {
+    printf("\n");
+    printf("          _    _ \n");
+    printf(" __ _____(_)__| |\n");
+    printf(" \\ V / _ \\ / _` |\n");
+    printf("  \\_/\\___/_\\__,_|\n");
+    printf("\n");
+    printf("\n");
+}
+
