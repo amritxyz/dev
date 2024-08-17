4b6b35ce9aa6ad27b84a271c601a9e960327bcfb
amrit-44404 <amrit44404@proton.me>
2024-08-17 14:36:06 +0545

    sum of natural numbers using recursion

---
+++ lab-report/17-rec_natural.c
@@ -0,0 +1,19 @@
+/* WAP to print SUM of natural numbers using recursion*/
+#include"stdio.h"
+int natural(int);
+int natural(int a){
+	if (a==1){
+		return 1;
+	}
+	else {
+		return natural(a-1)+a;
+	}
+}
+int main(){
+	int a;
+	printf("Sum of natural numbers up to: ");
+	scanf("%d", &a);
+	printf("The sum of natural numbers up to %d = %d\n", a, natural(a));
+	return a;
+}
+
