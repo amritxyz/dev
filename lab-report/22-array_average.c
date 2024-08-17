4e796694772c77f9eda9e0b00d6ed0674e6f9b24
amrit-44404 <amrit44404@proton.me>
2024-08-17 18:16:31 +0545

    average and total marks of 5 subjects

---
+++ lab-report/22-array_average.c
@@ -0,0 +1,26 @@
+/* WAP to find student's Total and Average marks in 5 subjects */
+#include <stdio.h>
+
+int main() {
+    int arr[5];
+    int sum = 0;
+    float average;
+
+    printf("Enter 5 integers:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Subject %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < 5; i++) {
+        sum += arr[i];
+    }
+
+    average = (float)sum / 5;
+
+    printf("Total sum of the 5 numbers is: %d\n", sum);
+    printf("The average of the 5 numbers is: %.2f\n", average);
+
+    return 0;
+}
+
