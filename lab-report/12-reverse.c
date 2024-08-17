295c92475da67aab36a73016e4be4793668d32ec
amrit-44404 <amrit44404@proton.me>
2024-08-17 12:52:29 +0545

    reverse number

---
+++ lab-report/12-reverse.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int reverseNumber(int n) {
+    int reversed = 0, remainder;
+
+    while (n != 0) {
+        remainder = n % 10;
+        reversed = reversed * 10 + remainder;
+        n /= 10;
+    }
+
+    return reversed;
+}
+
+int main() {
+    int number, reversed;
+
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    reversed = reverseNumber(number);
+
+    printf("Reversed number: %d\n", reversed);
+
+    return 0;
+}
+
