4d36c9955db1daa12ab201054013a0d24cc491f8
amrit-44404 <amrit44404@proton.me>
2024-08-21 12:03:55 +0545

    new prac

--- for-loop/prac.c
+++ for-loop/prac.c
@@ -1,14 +1,15 @@
-#include"stdio.h"
+#include<stdio.h>
 int main(){
-    int i, j;
-    for (i=5;i>=1;i--){
-         for (j=1;j<=i;j++)
-         {
-            j=j*10 + 1;
-            printf("%d\t", j);
-         }
-         printf("\n");
-    }
-    return 0;
+	int a=0, b=1, c=0, n;
+	printf("Enter the number of terms: ");
+	scanf("%d", &n);
+	for (int i=1; i<=n; i++){
+		printf("%d\t", a);
+		c=a+b;
+		a=b;
+		b=c;
+	}
+	printf("\n");
+	return 0;
 }
 
