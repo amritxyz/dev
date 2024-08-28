a3821396445508061a37fa73e789b8b971604326
amrit-44404 <amrit44404@proton.me>
2024-08-28 23:53:29 +0545

    new matrices solutions

---
+++ c/array/13-lower_diagonal.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+	int a[3][3], i, j, sum=0;
+	printf("Enter the numbers for matrix\n");
+
+	for (i=0; i<3; i++){
+		for (j=0; j<3; j++)
+			scanf("%d", &a[i][j]);
+	}
+
+	printf("The matrix is:\n");
+	for (i=0; i<3; i++){
+		for (j=0; j<3; j++)
+			printf("%d\t", a[i][j]);
+		printf("\n");
+	}
+	for (i=0; i<3; i++){
+		for (j=0; j<3; j++){
+			if (i>j)
+				sum+=a[i][j];
+		}
+	}
+
+	printf("The sum of lower diagonal of above matrix is: %d\n", sum);
+	return 0;
+}
+
