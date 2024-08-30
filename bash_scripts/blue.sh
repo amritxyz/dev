87a48883db1abd42d371f482b3414e866ef5a30b
amrit-44404 <amrit44404@proton.me>
2024-08-30 22:40:52 +0545

    minor

--- bash_scripts/blue.sh
+++ bash_scripts/blue.sh
@@ -1,4 +1,4 @@
-##!/bin/sh
+#!/bin/sh
 
 ## Scan for devices
 #echo "Scanning for Bluetooth devices..."
@@ -14,14 +14,14 @@ get_device_list() {
 device_list=$(get_device_list)
 selected_device=$(echo "$device_list" | dmenu -l 10 | awk '{print $1}')
 if [ -z "$selected_device" ]; then
-    echo "No device selected."
+    notify-send "No device selected."
     exit 1
 fi
 
 # Get the MAC address of the selected device
 MAC=$(echo "$device_list" | grep "$selected_device" | awk '{print $2}')
 if [ -z "$MAC" ]; then
-    echo "Failed to find MAC address for selected device."
+    notify-send "Failed to find MAC address for selected device."
     exit 1
 fi
 
@@ -30,20 +30,20 @@ connect=$(bluetoothctl info "$MAC" | grep Connected: | awk '{print $2}')
 
 # Attempt to connect or disconnect based on the connection status
 if [ "$connect" = "no" ]; then
-    echo "Attempting to connect to $selected_device..."
+    notify-send "Attempting to connect to $selected_device..."
     if bluetoothctl connect "$MAC"; then
-        echo "Successfully connected to $selected_device"
+        notify-send "Successfully connected to $selected_device"
     else
-        echo "Failed to connect to $selected_device"
+        notify-send "Failed to connect to $selected_device"
     fi
 elif [ "$connect" = "yes" ]; then
-    echo "Attempting to disconnect from $selected_device..."
+    notify-send "Attempting to disconnect from $selected_device..."
     if bluetoothctl disconnect "$MAC"; then
-        echo "Successfully disconnected from $selected_device"
+       notify-send "Successfully disconnected from $selected_device"
     else
-        echo "Failed to disconnect from $selected_device"
+       notify-send "Failed to disconnect from $selected_device"
     fi
 else
-    echo "Unable to determine connection status for $selected_device."
+     notify-send "Unable to determine connection status for $selected_device."
     exit 1
 fi
