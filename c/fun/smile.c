4e393d9df22dc6e0c2e45093ab7b44d636fd076c
amrit-44404 <amrit44404@proton.me>
2024-09-02 22:57:14 +0545

    a minor fun program

---
+++ c/fun/smile.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    printf("  _____  \n");
+    printf(" /     \\ \n");
+    printf("|  O O  |\n");
+    printf("|   ^   |\n");
+    printf("|  \\_/  |\n");
+    printf(" \\_____/ \n");
+    printf("   | |   \n");
+    printf("   | |   \n");
+    printf("   | |   \n");
+    printf("  /   \\  \n");
+    printf(" /     \\ \n");
+    printf(" \\/___\\/ \n");
+
+    return 0;
+}
+
