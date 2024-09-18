648b5efd5a272ea72caae376157ca38203c486ae
nyx-void <amrit44404@proton.me>
2024-09-18 06:56:14 +0545

    new day new work

---
+++ c/pointers/4-func.c
@@ -0,0 +1,15 @@
+/* Function Pointer as Function Argument */
+#include<stdio.h>
+int compute(int(*operation)(int, int), int x, int y){
+	printf("Result: %d\n", operation(x, y));
+}
+int multiply(int a, int b){
+	return a*b;
+}
+int main(int a, int b){
+	printf("Enter any two numbers:\n");
+	scanf("%d%d", &a, &b);
+	compute(multiply, a, b);
+	return 0;
+}
+
