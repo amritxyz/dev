274d093dd095a5fbac4e6eb2a18852658642c7f1
nyx-void <amrit44404@proton.me>
2024-09-19 12:14:15 +0545

    new day new work

---
+++ web/7-day/script.js
@@ -0,0 +1,20 @@
+// function me() {
+// 	this.name="Amrit";
+// 	this.addr="Devinagar";
+// 	this.nothing="nothing";
+// }
+// const hell = new me();
+// 	console.log(hell);
+
+function handleClick() {
+	alert("You clicked the button");
+}
+function handleHover() {
+	alert("You Hovered the button");
+}
+function handleOut() {
+	alert("You Leaved the button");
+}
+function handleLoad() {
+	alert("The page has been successfully loaded");
+}
