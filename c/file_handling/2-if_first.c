f8d8b8f2932a42e541a41eac0beba1eebda5b237
nyx-void <amrit44404@proton.me>
2024-09-21 10:18:53 +0545

    new topic

---
+++ c/file_handling/2-if_first.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main() {
+	FILE *ptr;
+	ptr = fopen("Amrit.", "r");
+	int num;
+	if(ptr == NULL) {
+		printf("Sorry the file doesn't exists!\n");
+		return 1;
+	}
+	else {
+		fscanf(ptr,"%d", &num);
+		printf("The value of num is %d\n", num);
+		fclose(ptr);
+		return 0;
+	}
+}
+
