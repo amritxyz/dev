2eec9f8e6ed9ff4e1c472835acb85def0d0067a0
nyx-void <amrit44404@proton.me>
2024-11-16 22:43:13 +0545

    when the terminal resizes the stopwatch will always appear in the center

--- projects/digital-clock_c/main.c
+++ projects/digital-clock_c/main.c
@@ -3,8 +3,10 @@
 #include <time.h>
 #include <unistd.h>
 
-void clear_screen_and_move_cursor(int row, int col) {
-    // Move the cursor to the desired row and column
+void
+clear_screen_and_move_cursor(int row, int col)
+{
+    /* Move the cursor to the desired row and column */
     printf("\033[%d;%dH", row, col);
     fflush(stdout);
 }
@@ -18,23 +20,23 @@ int main(void)
 
     while (1)
     {
-        // Get the terminal size using the `stty` command (every time before updating)
+        /* Get the terminal size using the `stty` command (every time before updating) */
         FILE *fp = popen("stty size", "r");
         fscanf(fp, "%d %d", &rows, &cols);
         fclose(fp);
 
         int center_row = rows / 2;
-        int center_col = (cols - 10) / 2; // 10 is the length of "XX:XX:XX" (time string)
+        int center_col = (cols - 10) / 2; /* 10 is the length of "XX:XX:XX" (time string) */
 
-        // Clear the terminal and move the cursor to the center
+        /* Clear the terminal and move the cursor to the center */
         system("clear");
         clear_screen_and_move_cursor(center_row, center_col);
 
-        // Print the current time
+        /* Print the time */
         printf("%02d : %02d : %02d", hour, minute, second);
         fflush(stdout);
 
-        // Update the stopwatch time
+        /* Update the stopwatch time */
         second++;
         if (second == 60) {
             minute++;
@@ -51,8 +53,6 @@ int main(void)
             minute = 0;
             second = 0;
         }
-
-        // Sleep for 1 second
         sleep(1);
     }
 
