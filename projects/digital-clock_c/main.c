1464a7395bde0d70857a0128604fb5a1c2796e67
nyx-void <amrit44404@proton.me>
2024-11-17 07:17:08 +0545

    sapce swaped with tabs

--- projects/digital-clock_c/main.c
+++ projects/digital-clock_c/main.c
@@ -6,56 +6,56 @@
 void
 clear_screen_and_move_cursor(int row, int col)
 {
-    /* Move the cursor to the desired row and column */
-    printf("\033[%d;%dH", row, col);
-    fflush(stdout);
+	/* Move the cursor to the desired row and column */
+	printf("\033[%d;%dH", row, col);
+	fflush(stdout);
 }
 
 int main(void)
 {
-    int hour, minute, second;
-    hour = minute = second = 0;
+	int hour, minute, second;
+	hour = minute = second = 0;
 
-    int rows, cols;
+	int rows, cols;
 
-    while (1)
-    {
-        /* Get the terminal size using the `stty` command (every time before updating) */
-        FILE *fp = popen("stty size", "r");
-        fscanf(fp, "%d %d", &rows, &cols);
-        fclose(fp);
+	while (1)
+	{
+		/* Get the terminal size using the `stty` command (every time before updating) */
+		FILE *fp = popen("stty size", "r");
+		fscanf(fp, "%d %d", &rows, &cols);
+		fclose(fp);
 
-        int center_row = rows / 2;
-        int center_col = (cols - 10) / 2; /* 10 is the length of "XX:XX:XX" (time string) */
+		int center_row = rows / 2;
+		int center_col = (cols - 10) / 2; /* 10 is the length of "XX:XX:XX" (time string) */
 
-        /* Clear the terminal and move the cursor to the center */
-        system("clear");
-        clear_screen_and_move_cursor(center_row, center_col);
+		/* Clear the terminal and move the cursor to the center */
+		system("clear");
+		clear_screen_and_move_cursor(center_row, center_col);
 
-        /* Print the time */
-        printf("%02d : %02d : %02d", hour, minute, second);
-        fflush(stdout);
+		/* Print the time */
+		printf("%02d : %02d : %02d", hour, minute, second);
+		fflush(stdout);
 
-        /* Update the stopwatch time */
-        second++;
-        if (second == 60) {
-            minute++;
-            second = 0;
-        }
+		/* Update the stopwatch time */
+		second++;
+		if (second == 60) {
+			minute++;
+			second = 0;
+		}
 
-        if (minute == 60) {
-            hour++;
-            minute = 0;
-        }
+		if (minute == 60) {
+			hour++;
+			minute = 0;
+		}
 
-        if (hour == 24) {
-            hour = 0;
-            minute = 0;
-            second = 0;
-        }
-        sleep(1);
-    }
+		if (hour == 24) {
+			hour = 0;
+			minute = 0;
+			second = 0;
+		}
+		sleep(1);
+	}
 
-    return 0;
+	return 0;
 }
 
