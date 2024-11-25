4bc7b8e58d55c8fe6d74db4b12ca7d2953bf0802
nyx-void <amrit44404@proton.me>
2024-11-26 00:36:21 +0545

    minor change

--- rust/2-prime.rs
+++ rust/2-prime.rs
@@ -1,17 +1,17 @@
 fn is_prime(n: u32) -> bool {
     if n <= 1 {
-        return false; // Numbers less than or equal to 1 are not prime
+        return false;
     }
     for i in 2..=(n as f64).sqrt() as u32 {
         if n % i == 0 {
-            return false; // n is divisible by i, so it's not prime
+            return false;
         }
     }
-    true // n is prime
+    true // Prime
 }
 
 fn main() {
-    let n = 29; // Example number to check
+    let n = 29;
     if is_prime(n) {
         println!("{} is prime", n);
     } else {
