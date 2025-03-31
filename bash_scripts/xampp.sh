04566a0e48f1fb5427dd937b9686dcb7e30aa686
amritxyz <amrit44404@proton.me>
2025-03-31 12:58:11 +0545

    install xampp and run on local machine

---
+++ bash_scripts/xampp.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Install Dependencies First
+sudo pacman -S lib32-gcc-libs lib32-glibc lib32-libgcrypt
+sudo pacman -S libxcrypt-compat
+sudo pacman -S net-tools
+sudo pacman -S inetutils
+
+# Post Installation
+cd /opt/
+wget https://sourceforge.net/projects/xampp/files/XAMPP%20Linux/8.2.12/xampp-linux-x64-8.2.12-0-installer.run
+sudo chown $USER:$USER /opt/xampp-linux-x64-8.2.12-0-installer.run
+sudo /opt/xampp-linux-x64-8.2.12-0-installer.run
