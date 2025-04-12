abb8035161320b1240beaf8922efeac3b6e374ec
amritxyz <amrit44404@proton.me>
2025-04-12 22:32:33 +0545

    added ncurses stuffs and more

--- projects/digital-clock_c/main.c
+++ projects/digital-clock_c/main.c
@@ -17,64 +17,56 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <unistd.h>
+#include <ncurses.h>
 
-static void clear_screen_and_move_cursor(int, int);
-
-static void
-clear_screen_and_move_cursor(int row, int col)
+int main(void)
 {
-	/* Move the cursor to the desired row and column */
-	printf("\033[%d;%dH", row, col);
-	fflush(stdout);
-}
+    int hour = 0, minute = 0, second = 0;
+    int rows, cols;
 
-int
-main(void)
-{
-	int hour, minute, second;
-	hour = minute = second = 0;
+    // Initialize ncurses
+    initscr();            // Initialize screen
+    timeout(1000);        // Set 1 second timeout for non-blocking input
+    noecho();             // Disable input echo
+    curs_set(0);          // Hide cursor
 
-	int rows, cols;
+    // Get terminal size
+    getmaxyx(stdscr, rows, cols);
+    int center_row = rows / 2;
+    int center_col = (cols - 10) / 2;
 
-	while (1)
-	{
-		/* Get the terminal size using the `stty` command (every time before updating) */
-		FILE *fp = popen("stty size", "r");
-		fscanf(fp, "%d %d", &rows, &cols);
-		fclose(fp);
+    while (1)
+    {
+        // Move cursor to the center
+        move(center_row, center_col);
+        clrtoeol(); // Clear the current line
 
-		int center_row = rows / 2;
-		int center_col = (cols - 10) / 2; /* 10 is the length of "XX:XX:XX" (time string) */
+        // Print the time
+        printw("%02d : %02d : %02d", hour, minute, second);
+        refresh();
 
-		/* Clear the terminal and move the cursor to the center */
-		system("clear");
-		clear_screen_and_move_cursor(center_row, center_col);
+        // Update stopwatch time
+        second++;
+        if (second == 60) {
+            minute++;
+            second = 0;
+        }
 
-		/* Print the time */
-		printf("%02d : %02d : %02d", hour, minute, second);
-		fflush(stdout);
+        if (minute == 60) {
+            hour++;
+            minute = 0;
+        }
 
-		/* Update the stopwatch time */
-		second++;
-		if (second == 60) {
-			minute++;
-			second = 0;
-		}
+        if (hour == 24) {
+            hour = 0;
+            minute = 0;
+            second = 0;
+        }
 
-		if (minute == 60) {
-			hour++;
-			minute = 0;
-		}
+        sleep(1);
+    }
 
-		if (hour == 24) {
-			hour = 0;
-			minute = 0;
-			second = 0;
-		}
-		sleep(1);
-	}
-
-	return EXIT_FAILURE;
+    endwin();
+    return EXIT_SUCCESS;
 }
