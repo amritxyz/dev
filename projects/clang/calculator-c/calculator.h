a09874f447e4368c97630f4574b10cd936db1e55
amritxyz <amrit44404@proton.me>
2025-04-12 23:07:00 +0545

    projects: space for other langs also

---
+++ projects/clang/calculator-c/calculator.h
@@ -0,0 +1,10 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+/* prototypes */
+int sum(float, float);
+int sub(float, float);
+int mul(float, float);
+int dev(float, float);
+
+#endif /* CALCULATOR_H */
