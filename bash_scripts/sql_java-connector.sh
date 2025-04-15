47231bea2fefe4eb1d3cb96c8b0fa8e990f84936
amritxyz <amrit44404@proton.me>
2025-04-15 13:55:37 +0545

    some more helpful things

--- bash_scripts/sql_java-connector.sh
+++ bash_scripts/sql_java-connector.sh
@@ -5,3 +5,9 @@ cd /opt
 sudo wget https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-j-9.3.0.tar.gz
 sudo tar -xzf mysql-connector-j-9.3.0.tar.gz
 sudo rm -rf mysql-connector-j-9.3.0.tar.gz
+
+# Path to the Java-DataBase-Connection Driver
+# /opt/mysql-connector-j-9.3.0/src/legacy/java/com/mysql/jdbc
+#
+# In Java
+# Class.forName("com.mysql.cj.jdbc.Driver");
