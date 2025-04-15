a99cb1df702050d86316261bdf7f82104cb5e68d
amritxyz <amrit44404@proton.me>
2025-04-15 13:47:34 +0545

    install connector

---
+++ bash_scripts/sql_java-connector.sh
@@ -0,0 +1,7 @@
+# MySQL
+
+# Installation
+cd /opt
+sudo wget https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-j-9.3.0.tar.gz
+sudo tar -xzf mysql-connector-j-9.3.0.tar.gz
+sudo rm -rf mysql-connector-j-9.3.0.tar.gz
